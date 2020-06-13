e82317c3f9717acfc75cdf2e0615e07b8b16a285
Jihoon Jeon <junji636@gmail.com>
2020-06-13 15:02:00 +0900

    js home contact push

--- main.js
+++ main.js
@@ -10,3 +10,29 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("nav--dark");
   }
 });
+
+//Handle scrolling when tapping on the nav menu
+
+const navbarmenu = document.querySelector(".navbar__menu");
+navbarmenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+
+  if (link == null) {
+    return;
+  }
+
+  scrollIntoViews(link);
+});
+
+//Handle Scrolling when tapping contact me button
+
+const contactBtn = document.querySelector(".home__contact");
+contactBtn.addEventListener("click", () => {
+  scrollIntoViews("#contact");
+});
+
+function scrollIntoViews(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
