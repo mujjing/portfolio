40ff297fc10b2896c16b6bf30e3ea56f67c5f3f4
Jihoon Jeon <junji636@gmail.com>
2020-06-13 16:14:57 +0900

    js css work filtering

--- main.js
+++ main.js
@@ -55,6 +55,42 @@ arrowUp.addEventListener("click", () => {
   scrollIntoViews("#home");
 });
 
+//project
+const workBtnContainer = document.querySelector(".work__categories");
+const projectContainer = document.querySelector(".work__projects");
+const projects = document.querySelectorAll(".project");
+workBtnContainer.addEventListener("click", (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  console.log(filter);
+  if (filter == null) {
+    return;
+  }
+
+  projectContainer.classList.add("anim-out");
+  setTimeout(() => {
+    projects.forEach((project) => {
+      console.log(project.dataset.type);
+      if (filter === "*" || filter === project.dataset.type) {
+        project.classList.remove("invisible");
+      } else {
+        project.classList.add("invisible");
+      }
+    });
+    projectContainer.classList.remove("anim-out");
+  }, 300);
+
+  //   console.log("----------------------");
+  //   for (let project of projects) {
+  //     console.log(project);
+  //   }
+  //   console.log("----------------------");
+
+  //   for (let i = 0; i < projects.length; i++) {
+  //     project = projects[i];
+  //     console.log(project);
+  //   }
+});
+
 function scrollIntoViews(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
